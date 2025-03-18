dfc8b8da0a2836fb87b5645ef1894f5bf0975a4a
Huyenhuyencodoicocap <142354967+HuyenHuyencodoicocap@users.noreply.github.com>
2025-03-18 18:34:14 +0700

    add attribute of board

--- ModelScript/Board.js
+++ ModelScript/Board.js
@@ -2,17 +2,25 @@ class Board {
     constructor() {
         this.img=PoolGame.getInstance().assets.images["board"];
         
-        this.HolePosition = []; // Mảng vị trí lỗ (Vector2D[])
-        this.HoleRadius = 55; // Bán kính lỗ (const float)
+        this.HolePosition = [
+            new Vector2D(73,73),    //trên trái
+            new Vector2D(1427,73),  //trên phải
+            new Vector2D(73,753),   //dưới trái
+            new Vector2D(1427,753), //dưới phải
+            new Vector2D(750,52),   //trên giữa
+            new Vector2D(750,772),  //dưới giữa
+
+        ]; // Mảng vị trí lỗ (Vector2D[])
+        this.HoleRadius = 64; // Bán kính lỗ (const float)
         this.width = this.img.width; // Chiều rộng (const float)
         this.height = this.img.height; // Chiều cao (const float)
         
 
         // Viền của bàn chơi
-        this.topWall = 0;
-        this.leftWall = 0;
-        this.bottomWall = 0;
-        this.rightWall = 0;
+        this.topWall = 60;
+        this.leftWall = 60;
+        this.bottomWall = 60;
+        this.rightWall = 60;
 
         //Không đucợ thay đổi thông số của bàn bắn
         Object.freeze(this);
