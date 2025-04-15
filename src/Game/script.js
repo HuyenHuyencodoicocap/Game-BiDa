11322299c69b71be8d66dd70a89c03ef3eb0437f
HuyenHuyencodoicocap <1632004huyen@gmail.com>
2025-04-16 03:37:08 +0700

    add btn playagain, btnHome

--- src/Game/script.js
+++ src/Game/script.js
@@ -1,1 +1,9 @@
 
+    document.getElementById("playAgainBtn").addEventListener("click", function () {
+        location.reload();
+    });
+
+    document.getElementById("homeBtn").addEventListener("click", function () {
+        window.location.href = "../Home/index.html"; 
+    });
+
